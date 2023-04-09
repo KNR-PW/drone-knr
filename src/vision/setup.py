6a60943507e8f75159010feabab1fe33bee9ba72
qbaaa-0 <ntfr123@gmail.com>
2023-04-09 13:18:29 +0200

    create receive camera node

--- src/vision/setup.py
+++ src/vision/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'receive_gz_camera = vision.receiveGzCamera:main'
+            'receive_camera = vision.receiveCamera:main'
         ],
     },
 )
