05093f8a706d1d20867e84253be35d3ef7f3408e
qbaaa-0 <ntfr123@gmail.com>
2023-04-10 22:34:34 +0200

    add sim launchfile

---
+++ launch/drone_sim_launch.py
@@ -0,0 +1,19 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='vision',
+            executable='receive_camera',
+        ),
+        Node(
+            package='ros_gz_image',
+            executable='image_bridge',
+            parameters='/camera',
+            remappings=[
+                ('/input/pose', '/turtlesim1/turtle1/pose'),
+                ('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel'),
+            ]
+        )
+    ])
