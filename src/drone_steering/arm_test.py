23641724eca35de75e675249bdc4ae072ddc0d01
Max326 <maxconnor326@gmail.com>
2023-05-15 11:48:32 +0200

    delta dist instead of waiting added

--- src/drone_steering/arm_test.py
+++ src/drone_steering/arm_test.py
@@ -114,39 +114,13 @@ def get_distance_metres(aLocation1, aLocation2):
     dlat = aLocation2.lat - aLocation1.lat 
     dlong = aLocation2.lon - aLocation1.lon
     return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
+
+
+def get_distance_metres_ned(aLocation1, aLocation2):
+    dnorth = aLocation2.north - aLocation1.north
+    deast = aLocation2.east - aLocation1.east
+    return math.sqrt((dnorth*dnorth) + (deast*deast))
  
-
-
-def get_location_metres(original_location, dNorth, dEast):
-    """
-    Returns a LocationGlobal object containing the latitude/longitude `dNorth` and `dEast` metres from the 
-    specified `original_location`. The returned LocationGlobal has the same `alt` value
-    as `original_location`.
-
-    The function is useful when you want to move the vehicle around specifying locations relative to 
-    the current vehicle position.
-
-    The algorithm is relatively accurate over small distances (10m within 1km) except close to the poles.
-
-    For more information see:
-    http://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
-    """
-    earth_radius = 6378137.0 #Radius of "spherical" earth
-    #Coordinate offsets in radians
-    dLat = dNorth/earth_radius
-    dLon = dEast/(earth_radius*math.cos(math.pi*original_location.lat/180))
-
-    #New position in decimal degrees
-    newlat = original_location.lat + (dLat * 180/math.pi)
-    newlon = original_location.lon + (dLon * 180/math.pi)
-    if type(original_location) is LocationGlobal:
-        targetlocation=LocationGlobal(newlat, newlon,original_location.alt)
-    elif type(original_location) is LocationGlobalRelative:
-        targetlocation=LocationGlobalRelative(newlat, newlon,original_location.alt)
-    else:
-        raise Exception("Invalid Location object passed")
-        
-    return targetlocation
 
 # in global coordinate system, like you'd typically give someone coordinates
 # could be useful if our camera gave us coordinates in 'gps' style
@@ -189,18 +163,26 @@ def pos_change(north, east, down):
     # Get the vehicle's current position in the local frame of reference
     current_location = vehicle.location.local_frame
 
-    # next_location = LocationGlobal
+    next_location = LocationLocal
 
-    # next_location.lat = current_location.lat + north
-    # next_location.lon = current_location.lon + east
-    # next_location.alt = current_location.alt + down
+    next_location.north = current_location.north + north
+    next_location.east = current_location.east + east
+    next_location.down = current_location.down + down
 
-    # goto_position_target_local_ned(next_location.lat, next_location.lon, next_location.alt)
-    goto_position_target_local_ned(
-        vehicle.location.local_frame.north+north,
-         vehicle.location.local_frame.east+east, 
-         vehicle.location.local_frame.down+down)
-    time.sleep(10)
+    goto_position_target_local_ned(next_location.north, next_location.east, next_location.down)
+    # goto_position_target_local_ned(
+    #     vehicle.location.local_frame.north+north,
+    #      vehicle.location.local_frame.east+east, 
+    #      vehicle.location.local_frame.down+down)
+    
+
+    while vehicle.mode == "GUIDED":
+        remaining_distance = get_distance_metres_ned(vehicle.location.local_frame, next_location)
+        if remaining_distance <= 1:
+            print("Reached target waypoint")
+            break
+        time.sleep(1)
+
 
 
     # Get the vehicle's current position in the local frame of reference
