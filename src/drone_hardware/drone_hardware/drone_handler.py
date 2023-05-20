c657eead9afaac937053259cbcc40caa2c428190
StasiekKolodz <stas.kolodziejczyk@gmail.com>
2023-05-21 01:09:37 +0200

    .

--- src/drone_hardware/drone_hardware/drone_handler.py
+++ src/drone_hardware/drone_hardware/drone_handler.py
@@ -35,7 +35,7 @@ class DroneHandler(Node):
         ##CONNECT TO COPTER
 
 
-        connection_string = "/dev/serial0"
+        connection_string = "/dev/ttyS0"
 
         baud_rate = 57600
         self.get_logger().info("Connecting with copter...")
