b80ebc6e6b22d07db5749f1ff4b0c79d83521a30
StasiekKolodz <stas.kolodziejczyk@gmail.com>
2023-05-21 22:58:02 +0200

    .

--- src/drone_detector/drone_detector/image_publisher.py
+++ src/drone_detector/drone_detector/image_publisher.py
@@ -37,7 +37,7 @@ class ImagePublisher(Node):
 
         # Create a VideoCapture object
         # The argument '0' gets the default webcam.
-        self.img = cv2.imread('dumb.jpg', cv2.IMREAD_COLOR)
+        self.img = cv2.imread('/home/stas/Dron/drone_photos/drone_photosdrone_photo111.jpg', cv2.IMREAD_COLOR)
         self.cap = cv2.VideoCapture(
             "/home/stas/Dron/KNRDron/rosDron/install/drone_detector/lib/drone_detector/car_counting.mp4")
 
@@ -61,7 +61,7 @@ class ImagePublisher(Node):
             # The 'cv2_to_imgmsg' method converts an OpenCV
             # image to a ROS 2 image message
             frame = cv2.resize(frame, (640, 480), interpolation=cv2.INTER_LINEAR)
-            self.publisher_.publish(self.br.cv2_to_imgmsg(self.img))
+            # self.publisher_.publish(self.br.cv2_to_imgmsg(frame))
 
             # Display the message on the console
             self.get_logger().info('Publishing video frame')
@@ -69,6 +69,9 @@ class ImagePublisher(Node):
             self.cap.release()
             self.cap = cv2.VideoCapture(
                 "/home/stas/Dron/KNRDron/rosDron/install/drone_detector/lib/drone_detector/car_counting.mp4")
+        img = cv2.resize(self.img, (640, 480), interpolation=cv2.INTER_LINEAR)
+        img = cv2.blur(img, (10, 10))  
+        self.publisher_.publish(self.br.cv2_to_imgmsg(img))
 
 
 def main(args=None):
