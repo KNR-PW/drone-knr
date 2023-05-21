6d0d8ab8f507c4465cd5d5c48c4a3667f11cd842
StasiekKolodz <stas.kolodziejczyk@gmail.com>
2023-05-21 17:43:44 +0200

    .

--- src/drone_hardware/drone_hardware/drone_handler.py
+++ src/drone_hardware/drone_hardware/drone_handler.py
@@ -36,7 +36,7 @@ class DroneHandler(Node):
 
 
         connection_string = "/dev/serial0"
-        baud_rate = 57600
+        baud_rate = 921600
         self.get_logger().info("Connecting with copter...")
         self.vehicle = connect(connection_string, baud=baud_rate, wait_ready=False) #doesnt work with wait_ready=True
         self.state = "OK"
