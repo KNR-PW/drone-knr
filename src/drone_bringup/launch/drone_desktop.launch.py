9e0977dee78efbfa18a2fd6a35238861409bd170
StasiekKolodz <stas.kolodziejczyk@gmail.com>
2023-05-27 01:05:19 +0200

    added bringup

---
+++ src/drone_bringup/launch/drone_desktop.launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='drone_detector',
+            executable='image_subscriber',
+        ),
+        Node(
+            package='drone_gui',
+            executable='detector_gui',
+        ),
+        Node(
+            package='drone_gui',
+            executable='drone_control_gui',
+        ),
+    ])
