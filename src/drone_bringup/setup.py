74e7b5038233e6957c965bc877e53da08de61f9b
StasiekKolodz <stas.kolodziejczyk@gmail.com>
2023-05-27 01:15:18 +0200

    .

--- src/drone_bringup/setup.py
+++ src/drone_bringup/setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup
+from glob import glob
+import os
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
 
 package_name = 'drone_bringup'
 
@@ -10,6 +14,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
