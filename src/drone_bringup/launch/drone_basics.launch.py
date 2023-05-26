158084462e2a34308ce61625cb66e24052390567
StasiekKolodz <stas.kolodziejczyk@gmail.com>
2023-05-27 01:04:49 +0200

    added bringup pkg

---
+++ src/drone_bringup/launch/drone_basics.launch.py
@@ -0,0 +1,14 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='drone_detector',
+            executable='detector_server',
+        ),
+        Node(
+            package='drone_hardware',
+            executable='drone_handler',
+        )
+    ])
